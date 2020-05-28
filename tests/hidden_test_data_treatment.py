2c1c1b5484e49d9ba2ae07ccaa961850288c3ae9
Cesar Ramirez Perez <cramirez@ifae.es>
2020-05-28 03:50:13 -0700

    Fixed data treatment test

--- tests/hidden_test_data_treatment.py
+++ tests/hidden_test_data_treatment.py
@@ -17,19 +17,25 @@ class TestDataTreatment(unittest.TestCase):
             rmtree(data_treated_path)
     
     def test_data_treatment_without_bins(self):
-        data_treatment(self.sim_path, source=1)
+        data_treatment(self.sim_path, source=2,do_cls=True,do_kappa=True)
 
-        a = np.loadtxt(self.sim_path + f'/data_treated/source_1/mp_E.dat')
-
-        self.assertEqual(np.mean(a), -8.687597839307397e-09)
-        self.assertEqual(a[0], 3.161026775081331630e-04)
+        a = np.loadtxt(self.sim_path + f'/data_treated/source_2/mp_E.dat')
+        b = np.loadtxt(self.sim_path + f'/data_treated/source_2/cld_dd.dat')
+        d = np.loadtxt(self.sim_path + f'/data_treated/source_2/cld_kk.dat')
+    
+        self.assertEqual(np.mean(a), 1.3593664904906463e-11)
+        self.assertEqual(a[0]      , 1.0001937620402216e-05)
+        self.assertEqual(np.mean(b), 0.00530553916038216)
+        self.assertEqual(np.mean(d), 3.2987881842564064e-11)
+        
 
     def test_data_treatment_with_bins_2(self):
-        data_treatment(self.sim_path, source=1, minz=1.5, maxz=2.01, output_path = self.sim_path + f'/data_treated/source_1', do_cls=False, do_kappa=True)
+        # This will be saved as 200,201 in a regular basis. Just for testing pourpuses. In real usage It won't work well for this precision level
+        data_treatment(self.sim_path, source=2, minz=2, maxz=2.005, output_path = self.sim_path + f'/data_treated/binned/200_2005/source_2', do_cls=False, do_kappa=False)
 
-        a = np.loadtxt(self.sim_path + f'/data_treated/source_1/mp_E.dat')
+        a = np.loadtxt(self.sim_path + f'/data_treated/binned/200_2005/source_2/mp_E.dat')
 
-        self.assertEqual(a[0], 0.00031610267750813316)
+        self.assertEqual(np.mean(a), -4.153934804547951e-11)
 
 if __name__ == '__main__':
     unittest.main()
