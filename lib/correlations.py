dbbe9f004a6f19a1981051c455dcfd3410dc8243
Cesar Ramirez Perez <cramirez@ifae.es>
2020-05-28 09:25:22 -0700

    Save correlations to file

--- lib/correlations.py
+++ lib/correlations.py
@@ -1,7 +1,10 @@
-from lib.shear_reader import ShearReader
+from lib.shear_reader import ShearReader, redshift_to_str_for_path
 from lib.functions import check_iterable
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from shutil import copy2
+import os
+
 
 import logging
 log = logging.getLogger(__name__)
@@ -71,3 +74,34 @@ class CorrelateTwoShears:
             reg_intercept.append( model.intercept_)
         return np.mean(reg_coef), np.mean(reg_intercept)
         
+    def store_correlation(self, parameter='mp_e1', source=1, minz=None, maxz=None, out=None):
+        if out == None and len(self.sims) > 1:
+            raise ValueError('Multiple simulations provided. Output path is needed')
+        
+        if out == None:
+            out = self.path_to_correlation(parameter=parameter, source=source, minz=minz, maxz=maxz)
+            os.makedirs(out)
+            seed = list(self.seeds)[0]
+            copy2(self.sims[seed][1].location+'/sim_info.dat',out+'/compared_to_info.dat')         
+
+        correlation = self.correlation_in_bin(parameter=parameter, source=source, minz=minz, maxz=maxz)
+
+        with open(out+ '/' + parameter +'.dat', "w") as f:
+            f.write(str(correlation))
+
+    def get_correlation(self, parameter='mp_e1', source=1, minz=None, maxz=None):
+        file_path = self.path_to_correlation(parameter=parameter, source=source, minz=minz, maxz=maxz)
+
+        with open(file_path + parameter + '.dat',"r") as f:
+            correlation = f.read()
+        return float(correlation)
+
+    def path_to_correlation(self, parameter='mp_e1', source=1, minz=None, maxz=None):
+        seed = list(self.seeds)[0]
+        if minz != None and maxz != None:
+            minz_str = redshift_to_str_for_path(minz)
+            maxz_str = redshift_to_str_for_path(maxz)
+            out = self.sims[seed][0].location + f'/data_treated/correlations/' + self.sims[seed][1].preparation_time +f'/binned/{ minz_str }_{ maxz_str }/source_{ source }/'
+        else:
+            out = self.sims.values()[0].location + f'/data_treated/correlations/source_{ source }/' + self.sims.values()[1].preparation_time +'/'
+        return out
