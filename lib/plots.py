63276dc49b75d05d0f739147df1e54c2b276ff67
Cesar Ramirez Perez <cramirez@ifae.es>
2020-05-28 08:08:15 -0700

    Plots modified

--- lib/plots.py
+++ lib/plots.py
@@ -16,7 +16,7 @@ class ClsPlotter():
     colors = ['r','y','c','m']
 
     @classmethod
-    def plot(cls,ld, values, ax=None, colors=None, labels=None, alpha=None,  **kwargs):
+    def plot(cls,ld, values, ax=None, colors=None, labels=None, alpha=None, mask=None, **kwargs):
         ''' Plotting the cls with defined format.k
 
         Parameters:
@@ -24,18 +24,21 @@ class ClsPlotter():
             values: 4-dim array containing the following cls: (cld_dd, cld_ee, cld_de, cld_bb)
             labels: Label to use, by default they are the ones corresponding with values
             colors: Colors to use for the values. None for default values.
-            kwargs: Additional arguments will be sent to the plot function.
             alpha: Alpha array to apply to the values. 
+            mask: Mask to apply to everything (exclude some values to be plotted)
+            kwargs: Additional kw arguments will be sent to the plot function.
         '''
         if not ax: ax = plt.gca()
         if not labels: labels = cls.labels
         if not colors: colors = cls.colors
         if not alpha: alpha = [1,1,1,1]
+        if not mask: mask = [True,True,True,True]
 
         if len(values) != 4:
             raise ValueError('Values should be a 4dim array')
 
         for i in range(len(values)):
+            if not mask[i]: continue
             ax.plot(ld, values[i], colors[i]+'-', label=labels[i],alpha=alpha[i], **kwargs)
 
         ax.set_xlabel('$\\ell$', fontsize=16)
